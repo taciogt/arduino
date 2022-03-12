57a2bf522e311590b3b50de1d2622711c5b00005
Tácio Tavares <taciogt@gmail.com>
2022-03-12 19:32:13 -0300

    leds for displaying humidity state

--- garden/garden.ino
+++ garden/garden.ino
@@ -3,38 +3,93 @@
 const int RS = 12, EN = 11, D4 = 10, D5 = 9, D6 = 8, D7 = 7;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
-const int pinHygrometer = A0;
+const int pin_hygrometer = A0;
+const int pin_is_wet = PIN6;
+const int pin_is_dry = PIN5;
+const int pin_is_humid = PIN4;
+const int pin_open_valve = PIN3;
 
 int umidity;
-float umidity_percentage; 
+float humidity_input;
+int humidity_percentage = 0;
+
+const int min_humidity = 40;
+const int max_umidity = 50;
+
+const int maximum_valve_open_seconds = 5 * 1;  // 5 seconds, TODO: change to 5 minutes
+bool valve_is_open = false;
+
 
 void setup() {
     Serial.begin(9600);
 
-    pinMode(pinHygrometer, INPUT);
+    pinMode(pin_hygrometer, INPUT);
+    pinMode(pin_is_wet, OUTPUT);
+    pinMode(pin_is_humid, OUTPUT);
+    pinMode(pin_is_dry, OUTPUT);
+    pinMode(pin_open_valve, OUTPUT);
 
     lcd.begin(16, 2);
 
     lcd.clear();
 
     lcd.setCursor(0, 0);
-    lcd.print("Kit Avancado RC");
+    lcd.print("Monitor Jardim");
 
     Serial.println("starting lcd");
     Serial.println("starting umidity sensor");
 }
 
 void loop() {
-    umidity = analogRead(pinHygrometer);
+    umidity = analogRead(pin_hygrometer);
     Serial.print("umidity: ");
     Serial.println(umidity);
-    umidity_percentage = float(1023-umidity)/1023;
+    humidity_input = float(1023 - umidity) / 1023;
+    humidity_percentage = int(humidity_input * 100);
+
+    Serial.print("humidity input: ");
+    Serial.println(humidity_input);
     Serial.print("umidity percentage: ");
-    Serial.println(umidity_percentage);
+    Serial.println(humidity_percentage);
+    Serial.print("min_humidity: ");
+    Serial.println(min_humidity);
 
     // Serial.println(umidity);
 
     unsigned long tempo_decorrido = millis() / 1000;
+
+
+
+    if (humidity_percentage > max_umidity) {
+        Serial.println("Soil is wet");
+        digitalWrite(pin_is_wet, HIGH);
+        digitalWrite(pin_is_humid, LOW);
+        digitalWrite(pin_is_dry, LOW);
+        digitalWrite(pin_open_valve, LOW);
+    }
+    else if (humidity_percentage > min_humidity) {
+        Serial.println("Soil is humidy");
+        digitalWrite(pin_is_wet, LOW);
+        digitalWrite(pin_is_humid, HIGH);
+        digitalWrite(pin_is_dry, LOW);
+        digitalWrite(pin_open_valve, LOW);
+    }
+    else {
+        Serial.println("Soil is dry");
+        digitalWrite(pin_is_wet, LOW);
+        digitalWrite(pin_is_humid, LOW);
+        digitalWrite(pin_is_dry, HIGH);
+
+        if (!valve_is_open) {
+            digitalWrite(pin_open_valve, HIGH);
+
+        }
+    }
+
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Monitor Jardim");
 
     lcd.setCursor(0, 1);
     lcd.print(tempo_decorrido);
@@ -45,7 +100,7 @@ void loop() {
     lcd.setCursor(8, 1);
     lcd.print("um: ");
 
-    lcd.print(int(umidity_percentage*100));
+    lcd.print(humidity_percentage);
     lcd.print("%");
 
 
